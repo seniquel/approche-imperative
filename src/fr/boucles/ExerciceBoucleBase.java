6554c0bc07f5d78ab83ea55304f4c98d67f6535a
Léo SENIQUE <leo.senique@gmail.com>
2020-06-05 17:06:51 +0200

    Amélioration de la présentation

--- src/fr/boucles/ExerciceBoucleBase.java
+++ src/fr/boucles/ExerciceBoucleBase.java
@@ -3,24 +3,28 @@ package fr.boucles;
 public class ExerciceBoucleBase {
 
 	public static void main(String[] args) {
-		//exo 1
 		int i;
+		//Affichez tous les nombres de 1 à 10
+		System.out.println("Tous les nombres de 1 à 10 :");
 		for (i=1 ; i<=10 ; i++ ) {
 			System.out.println(i);
 		}
 		
-		//exo 2
-		String nom = "Léo Sénique";
+		//Affichez 20 fois votre nom et votre prénom
+		System.out.println("Mon nom et prénom écrit 20 fois :");
+		String nom = "SENIQUE Léo";
 		for (i=0 ; i<20 ; i++) {
 			System.out.println(nom);
 		}
 		
-		//exo 3
+		//Affichez les éléments pairs de 2 à 100
+		System.out.println("Tous les éléments pairs de 2 à 100 :");
 		for (i=2 ; i<=100 ; i+=2) {
 			System.out.println(i);
 		}
 		
-		//exo 4
+		//Affichez les éléments impairs de 1 à 99
+		System.out.println("Tous les éléments impairs de 1 à 99");
 		for (i=1 ; i<=100 ; i+=2) {
 			System.out.println(i);
 		}
--- src/fr/boucles/ExerciceTableauEntier.java
+++ src/fr/boucles/ExerciceTableauEntier.java
@@ -4,19 +4,19 @@ public class ExerciceTableauEntier {
 
 	public static void main(String[] args) {
 		
-		//exo 1
+		//Déclarez un tableau d’entiers contenant tous les entiers de 1 à 10
 		int tab[] = {1,2,3,4,5,6,7,8,9,10};
 		
-		//exo 2
+		//Affichez le premier élément du tableau
 		System.out.println("premier élément du tableau: "+tab[0]);
 		
-		//exo 3
+		//Affichez la longueur du tableau en utilisant la propriété length
 		System.out.println("longueur du tableau : "+tab.length);
 		
-		//exo 4
+		//Affichez le dernier élément du tableau en utilisant la propriété length
 		System.out.println("dernier élément du tableau : "+tab[tab.length-1]);
 		
-		//exo 5
+		//Modifiez la valeur de l’élément d’index 4 et donnez lui la valeur 8
 		tab[4] = 8;
 		System.out.println("élément 4 du tableau : "+tab[4]);
 	}

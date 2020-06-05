9da2a8dd7eaaa3aa2340358d65e09ef4c22db77a
Léo SENIQUE <leo.senique@gmail.com>
2020-06-05 14:18:34 +0200

    ajout class approche-imperative (fin TP2)

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,23 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byte1 = 10;
+		short short1 = 3000;
+		int int1 = -200000;
+		long long1 = 300L;
+		float float1 = 1.5F;
+		double double1 = 1.234567D;
+		char char1 = 'a';
+		String string1 = "blabla";
+		boolean bool1 = true;
+	
+		System.out.println(string1);
+		
+		String randomString = "Voici le résultat d’un calcul :\n 1+5=6";
+		
+	}
+
+}
